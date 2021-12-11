712e68ed40b21d55815f3af8610157fbcf9cea28
kontai <cho.kontai@gmail.com>
2021-12-11 15:21:54 +0800

    1211

---
+++ 練習/Perl/draw.pm
@@ -0,0 +1,5 @@
+package draw;
+use strict;
+use warnings FATAL => 'all';
+
+1;
---
+++ 練習/demo1204/1.py
@@ -0,0 +1,26 @@
+# thisset = {"apple", "banana", "cherry"}
+# mylist = ["kiwi", "orange"]
+#
+# v=mylist+list(thisset)
+# v.append("2")
+# f=list(x.title() if x.isalnum() else x for x in v)
+# f.sort()
+# print(f)
+
+'''
+i=1
+while(i<6):
+    print(i)
+    if(i==3):
+        pass
+        # break
+    i+=1
+else:
+    print("i is no longer less than 3"```
+'''
+
+li=[]
+for i in range(1,100,2):
+    li.append(i)
+print(li)
+print(str.split(str(li)))
